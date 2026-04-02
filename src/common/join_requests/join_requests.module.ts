7208d598c99ed6d22b90be11dde1db7b75cd2a68
silvante <khamidov.ko@gmail.com>
2026-04-02 20:56:26 +0500

    OOPS

--- src/common/join_requests/join_requests.module.ts
+++ src/common/join_requests/join_requests.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { JoinRequestsController } from './join_requests.controller';
 import { JoinRequestsService } from './join_requests.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AppJwtModule } from 'src/jwt/jwt.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AppJwtModule],
   controllers: [JoinRequestsController],
   providers: [JoinRequestsService],
 })
