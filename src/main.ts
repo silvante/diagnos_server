bd8cbb476e44fbaf78072ddb9e4fce3c5727acaf
silvante <khamidov.ko@gmail.com>
2026-05-10 23:59:13 +0500

    Origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONT_ORIGIN],
+    // origin: [process.env.FRONT_ORIGIN],
+    origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
